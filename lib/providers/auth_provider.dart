14e351372ee395a75f37b7c7a7509ac588308954
Nguyen Van Toan <nguyenvantoanhii2k3@gmail.com>
2025-03-12 08:07:54 +0700

    update 11/3

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -41,28 +41,14 @@ class AuthProvider extends ChangeNotifier {
     String password,
     BuildContext context,
   ) async {
-    String? emailError = validateEmail(email);
-    String? passwordError = validatePassword(password);
-    if (emailError != null || passwordError != null) {
-      ToastHelper.showError(context, emailError ?? passwordError!);
-      return;
-    }
-
     _isLoading = true;
     notifyListeners();
 
     await Future.delayed(Duration(seconds: 2));
-    User? user = await _auth.login(email, password);
+    await _auth.login(email, password, context);
 
     _isLoading = false;
     notifyListeners();
-
-    if (user != null) {
-      ToastHelper.showSuccess(context, "Đăng nhập thành công");
-      Navigator.pushReplacementNamed(context, "/home");
-    } else {
-      ToastHelper.showWarning(context, "Email hoặc PassWord không đúng !");
-    }
   }
 
   Future<void> register(
@@ -71,38 +57,14 @@ class AuthProvider extends ChangeNotifier {
     String confirmPassword,
     BuildContext context,
   ) async {
-    String? emailError = validateEmail(email);
-    String? passwordError = validatePassword(password);
-    String? passwordConfirmError = validatePassword(confirmPassword);
-    if (emailError != null ||
-        passwordError != null ||
-        passwordConfirmError != null) {
-      ToastHelper.showError(
-        context,
-        emailError ?? passwordError ?? passwordConfirmError!,
-      );
-      return;
-    }
-    if (password != confirmPassword) {
-      ToastHelper.showWarning(context, "Mật khẩu không trùng khớp");
-      return;
-    }
-
     _isLoading = true;
     notifyListeners();
 
     await Future.delayed(Duration(seconds: 1));
-    User? user = await _auth.register(email, password);
+    await _auth.register(email, password, confirmPassword, context);
 
     _isLoading = false;
     notifyListeners();
-
-    if (user != null) {
-      ToastHelper.showSuccess(context, "Đăng ký thành công");
-      Navigator.pushReplacementNamed(context, "/login");
-    } else {
-      ToastHelper.showWarning(context, "Đăng ký thất bại");
-    }
   }
 
   Future<void> loginWithGoogle(BuildContext context) async {
@@ -137,25 +99,5 @@ class AuthProvider extends ChangeNotifier {
     notifyListeners();
 
     Navigator.pushNamedAndRemoveUntil(context, "/login", (route) => false);
-  }
-
-  String? validatePassword(String? value) {
-    if (value == null || value.isEmpty) {
-      return "Mật khẩu không được để trống";
-    }
-    if (value.length < 6) {
-      return "Mật khẩu phải có ít nhất 6 ký tự";
-    }
-    return null;
-  }
-
-  String? validateEmail(String email) {
-    if (email.isEmpty) {
-      return "Email không được để trống";
-    }
-    if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(email)) {
-      return "Email không hợp lệ";
-    }
-    return null;
   }
 }
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,13 +1,36 @@
 import 'package:doancuoiky/models/users.dart';
 import 'package:doancuoiky/repositories/auth_repository.dart';
 import 'package:doancuoiky/repositories/user_repository.dart';
+import 'package:doancuoiky/utils/toasthelper.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 class AuthService {
   final AuthRepository _auth = AuthRepository();
   final UserRepository _user = UserRepository();
 
-  Future<User?> register(String email, String password) async {
+  Future<void> register(
+    String email,
+    String password,
+    String confirmPassword,
+    BuildContext context,
+  ) async {
+    String? emailError = validateEmail(email);
+    String? passwordError = validatePassword(password);
+    String? passwordConfirmError = validatePassword(confirmPassword);
+    if (emailError != null ||
+        passwordError != null ||
+        passwordConfirmError != null) {
+      ToastHelper.showError(
+        context,
+        emailError ?? passwordError ?? passwordConfirmError!,
+      );
+      return;
+    }
+    if (password != confirmPassword) {
+      ToastHelper.showWarning(context, "Mật khẩu không trùng khớp");
+      return;
+    }
     User? user = await _auth.register(email, password);
     if (user != null) {
       Users newUser = Users(
@@ -19,12 +42,32 @@ class AuthService {
       );
 
       await _user.saveUser(newUser);
+      ToastHelper.showSuccess(context, "Đăng ký thành công");
+      Navigator.pushReplacementNamed(context, "/login");
+    } else {
+      ToastHelper.showWarning(context, "Đăng ký thất bại");
+      return;
     }
-    return user;
   }
 
-  Future<User?> login(String email, String password) async {
-    return _auth.login(email, password);
+  Future<void> login(
+    String email,
+    String password,
+    BuildContext context,
+  ) async {
+    String? emailError = validateEmail(email);
+    String? passwordError = validatePassword(password);
+    if (emailError != null || passwordError != null) {
+      ToastHelper.showError(context, emailError ?? passwordError!);
+      return;
+    }
+    User? user = await _auth.login(email, password);
+    if (user != null) {
+      ToastHelper.showSuccess(context, "Đăng nhập thành công");
+      Navigator.pushReplacementNamed(context, "/home");
+    } else {
+      ToastHelper.showWarning(context, "Email hoặc PassWord không đúng !");
+    }
   }
 
   Future<User?> signInWithGoogle() async {
@@ -47,5 +90,25 @@ class AuthService {
 
   Future<void> signOut() async {
     return _auth.signOut();
+  }
+
+  String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Mật khẩu không được để trống";
+    }
+    if (value.length < 6) {
+      return "Mật khẩu phải có ít nhất 6 ký tự";
+    }
+    return null;
+  }
+
+  String? validateEmail(String email) {
+    if (email.isEmpty) {
+      return "Email không được để trống";
+    }
+    if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(email)) {
+      return "Email không hợp lệ";
+    }
+    return null;
   }
 }
